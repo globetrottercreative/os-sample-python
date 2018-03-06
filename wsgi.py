2c1d10e8ad83c164e034d10b6845fac774387bf0
globetrottercreative <32233134+globetrottercreative@users.noreply.github.com>
2018-03-06 18:56:15 +1300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,40 @@
+import socket
+import threading
+import os
+
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "Hello World!"
+def GetFile(name, sock):
+    filename = sock.recv(1024)
+    if os.path.isfile(filename):
+        sock.send(bytes("EXISTS" + str(os.path.getsize(filename)), 'utf-8'))
+        userResponse = sock.recv(1024)
+        if userResponse[:2].decode('utf-8') == 'OK':
+            with open(filename, 'rb') as f:
+                bytesToSend = f.read(1024)
+                sock.send(bytesToSend)
+                while bytesToSend != "":
+                    bytesToSend = f.read(1024)
+                    sock.send(bytesToSend)
+    else:
+        sock.send(bytes("ERR", 'utf-8'))
+    sock.close()
 
+def Main():
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 8080
+    s.bind((host, port))
+    s.listen(5)
+    print("Server Started.")
+    while True:
+        c, addr = s.accept()
+        print("Client Connected: " + str(addr))
+        t = threading.Thread(target=GetFile, args=("retrThread", c))
+        t.start()
+    s.close()
+    
 if __name__ == "__main__":
     application.run()
